a50e19ca65d1371e945e94c89319a6b912afdc33
Guillem Serra Calahorra <guillem_serra@hotmail.com>
2023-02-24 06:50:35 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+# PoleCubataBot
+Bot de Telegram de la Pole Cubata para el grupo del Viñarock
+
+
+docker build -t pole-cubata-bot-game .
+docker run -it --rm --name pole-cubata-bot-game pole-cubata-bot-game
---
+++ config.py
@@ -0,0 +1,7 @@
+TELEGRAM_TOKEN = "6190017820:AAFvVW_AINBa55AArqLtyqYAOmXmgSuaY5o"
+APP = "polecubatabot"
+DB_MONGO_HOST="classical-sound-system.pi6ndqv.mongodb.net"
+DB_USER = "classical-sound-system"
+DB_PWD = "4MnGjXcSaclM5IQw"
+CHAT_ID=-1001129525875
+STARNDARD_POLE_SCORE=1
